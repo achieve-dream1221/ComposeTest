0b1b3d888b5aacfa57edb59ba75f917a55437fef
achieve_dream <wulei7217@gmail.com>
2022-12-08 18:16:04 +0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Jetpack compose的Material Design3入门
+
+| 这是参考光官网的一篇文字
---
+++ app/src/main/java/com/wulei/composetest/ui/theme/Theme.kt
@@ -0,0 +1,30 @@
+package com.wulei.composetest.ui.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+
+@Composable
+fun ComposeTestTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colorScheme = when {
+        dynamicColor && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        dynamicColor && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
+        darkTheme -> darkColorScheme()
+        else -> lightColorScheme()
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
